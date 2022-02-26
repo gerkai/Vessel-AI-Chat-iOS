cd34345b1f4ceefe7b7fbc9bd5f275caf5891359
gerkai <gerkai202@gmail.com>
2022-02-26 07:46:48 -0800

    View slides up when textfield is tapped so it is no longer blocked by keyboard

--- Vessel/Login/SignupEmailCheckingViewController.swift
+++ Vessel/Login/SignupEmailCheckingViewController.swift
@@ -7,19 +7,24 @@
 
 import UIKit
 
-class SignupEmailCheckingViewController: UIViewController
+class SignupEmailCheckingViewController: UIViewController, UITextFieldDelegate
 {
     @IBOutlet weak var googleAuthButton: UIButton!
     @IBOutlet weak var appleAuthButton: UIButton!
     @IBOutlet var formFields: [UITextField]!
     
-    static let websiteURL = "https://vesselhealth.com/pages"
-    static let privacyPolicyURL = "\(websiteURL)/privacy-policy.html"
-    static let termsOfServiceURL = "\(websiteURL)/terms-of-service.html"
+    // to store the current active textfield
+    var activeTextField : UITextField? = nil
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     
@@ -41,16 +46,73 @@ class SignupEmailCheckingViewController: UIViewController
     
     @IBAction func onPrivacyButtonTapped(_ sender: Any)
     {
+        self.view.endEditing(true)
         openInSafari(url: Constants.privacyPolicyURL)
     }
     
     @IBAction func onTermsButtonTapped(_ sender: Any)
     {
+        self.view.endEditing(true)
         openInSafari(url: Constants.termsOfServiceURL)
     }
     
     @IBAction func onContinueButtonTapped(_ sender: Any)
     {
         //checkEmail()
+    }
+    
+    //MARK: - textfield delegates
+    func textFieldDidBeginEditing(_ textField: UITextField)
+    {
+        self.activeTextField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        self.activeTextField = nil
+    }
+    
+    //MARK: - Handle sliding view up/down so textfield isn't blocked by keyboard
+    @objc func keyboardWillShow(notification: NSNotification)
+    {
+        
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else
+        {
+            
+            // if keyboard size is not available for some reason, dont do anything
+           return
+        }
+        
+        var shouldMoveViewUp = false
+        
+        // if active text field is not nil
+        if let activeTextField = activeTextField
+        {
+            
+            let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
+            let topOfKeyboard = self.view.frame.height - keyboardSize.height
+            
+            if bottomOfTextField > topOfKeyboard
+            {
+                shouldMoveViewUp = true
+            }
+        }
+        
+        if(shouldMoveViewUp)
+        {
+            self.view.frame.origin.y = 0 - keyboardSize.height
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification)
+    {
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func backgroundTap(_ sender: UITapGestureRecognizer)
+    {
+        // go through all of the textfield inside the view, and end editing thus resigning first responder
+        // ie. it will trigger a keyboardWillHide notification
+        self.view.endEditing(true)
     }
 }
