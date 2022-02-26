b41556ebe3356e5b015f93d0ca2c6b7ebc005768
gerkai <gerkai202@gmail.com>
2022-02-25 17:10:51 -0800

    Added components

---
+++ Vessel/Components/BounceButton.swift
@@ -0,0 +1,58 @@
+//
+//  BounceButton.swift
+//  Bloom
+//
+//  Created by Seth Sandler on 4/2/19.
+//  Copyright © 2019 Bloom. All rights reserved.
+//
+
+import UIKit
+
+public class BounceButton: UIButton {
+
+    private static let cornerRadius: CGFloat = 22.0
+    
+    @IBInspectable var scale: CGFloat = 0.95
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+
+    override public func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+    func setupView() {
+        //Bind event handlers for the button
+        addTarget(self, action: #selector(buttonDidClick), for: UIControl.Event.touchUpInside)
+        addTarget(self, action: #selector(buttonDidTouchDown), for: [UIControl.Event.touchDown, UIControl.Event.touchDragEnter])
+        addTarget(self, action: #selector(buttonDidCancel), for: [UIControl.Event.touchCancel, UIControl.Event.touchDragExit])
+    }
+
+    //------------------------------------------------------------------------------------------------------
+    //Event Handling
+    //------------------------------------------------------------------------------------------------------
+
+    @objc
+    func buttonDidTouchDown() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [UIView.AnimationOptions.curveEaseOut, UIView.AnimationOptions.allowUserInteraction], animations: {
+            self.layer.transform = CATransform3DMakeScale(self.scale, self.scale, 1)
+        }, completion: nil)
+    }
+
+    @objc
+    func buttonDidCancel() {
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [UIView.AnimationOptions.curveEaseOut, UIView.AnimationOptions.allowUserInteraction], animations: {
+            self.layer.transform = CATransform3DIdentity
+        }, completion: nil)
+    }
+
+    @objc
+    func buttonDidClick() {
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [UIView.AnimationOptions.curveEaseOut, UIView.AnimationOptions.allowUserInteraction], animations: {
+            self.layer.transform = CATransform3DIdentity
+        }, completion: nil)
+    }
+}
---
+++ Vessel/Components/VesselButton.swift
@@ -0,0 +1,33 @@
+//
+//  ArrowBounceButton.swift
+//  vessel-ios
+//
+//  Created by Paul Wong on 4/19/20.
+//  Copyright © 2020 Vessel Health Inc. All rights reserved.
+//
+
+import UIKit
+
+class VesselButton: BounceButton {
+
+    private static let cornerRadius: CGFloat = 22.0
+    private static let padding: CGFloat = 30.0
+
+    override func setupView() {
+        super.setupView()
+        
+        layer.cornerRadius = VesselButton.cornerRadius
+        
+        if let imageView = imageView, imageView.image != nil {
+            // only set title insets if there is an image
+            titleEdgeInsets = UIEdgeInsets(top: 0, left: 20.0, bottom: 0, right: 0)
+        }
+    }
+    
+    public override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
+        var imageFrame = super.imageRect(forContentRect: contentRect)
+        imageFrame.origin.x = bounds.maxX - imageFrame.width - VesselButton.padding
+        return imageFrame
+    }
+
+}
