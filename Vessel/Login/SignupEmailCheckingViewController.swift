4bb5cd16ac6da1e2b5a4f3721426f7f0fb0d2492
gerkai <gerkai202@gmail.com>
2022-07-12 18:57:48 -0700

    VH-2362: Fixed keyboard overlapping textfields. Also consolidated view scrolling with keyboard into a KeyboardFriendlyViewController class that viewControllers with textfields can subclass.

--- Vessel/Login/SignupEmailCheckingViewController.swift
+++ Vessel/Login/SignupEmailCheckingViewController.swift
@@ -7,30 +7,15 @@
 
 import UIKit
 
-class SignupEmailCheckingViewController: UIViewController, UITextFieldDelegate, SocialAuthViewDelegate
+class SignupEmailCheckingViewController: KeyboardFriendlyViewController, UITextFieldDelegate, SocialAuthViewDelegate
 {
     @IBOutlet weak var googleAuthButton: UIButton!
     @IBOutlet weak var appleAuthButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     
-    // to store the current active textfield
-    var activeTextField : UITextField? = nil
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
-        self.view.addGestureRecognizer(tapGestureRecognizer)
-        
-        // Do any additional setup after loading the view.
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
     
     @IBAction func onBackButtonPressed(_ sender: Any)
     {
-        //self.navigationController?.popViewController(animated: true)
         self.navigationController?.fadeOut()
     }
     
@@ -119,50 +104,6 @@ class SignupEmailCheckingViewController: UIViewController, UITextFieldDelegate, 
     func textFieldDidEndEditing(_ textField: UITextField)
     {
         self.activeTextField = nil
-    }
-    
-    //MARK: - Handle sliding view up/down so textfield isn't blocked by keyboard
-    @objc func keyboardWillShow(notification: NSNotification)
-    {
-        
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else
-        {
-            
-            // if keyboard size is not available for some reason, dont do anything
-           return
-        }
-        
-        var shouldMoveViewUp = false
-        
-        // if active text field is not nil
-        if let activeTextField = activeTextField
-        {
-            
-            let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
-            let topOfKeyboard = self.view.frame.height - keyboardSize.height
-            
-            if bottomOfTextField > topOfKeyboard
-            {
-                shouldMoveViewUp = true
-            }
-        }
-        
-        if(shouldMoveViewUp)
-        {
-            self.view.frame.origin.y = 0 - keyboardSize.height
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification)
-    {
-        self.view.frame.origin.y = 0
-    }
-    
-    @objc func backgroundTap(_ sender: UITapGestureRecognizer)
-    {
-        // go through all of the textfield inside the view, and end editing thus resigning first responder
-        // ie. it will trigger a keyboardWillHide notification
-        self.view.endEditing(true)
     }
     
     //MARK: - SocialAuth delegates
