ad0650d2d09bbbea6178de0470942bbb66bfa2d4
gerkai <gerkai202@gmail.com>
2022-07-17 00:20:08 -0500

    VH-2325: Added explanation for super properties, user properties, and identifying a user on the analytics protocol

--- Vessel/Analytics/Analytics.swift
+++ Vessel/Analytics/Analytics.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Nicolas Medina on 7/11/22.
 //
+//  Super properties are properties that once added the will be sent on every posterior called event
+//  User properties are properties assigned for an specific user but aren't from an event
+//  By identifying you can assign an Id to the user, so events are shared between devices (Commonly the DB id for the user) 
+//  This is by Mixpanel design, here is more documentation on this: https://developer.mixpanel.com/docs/swift
+//
 
 import Foundation
 
