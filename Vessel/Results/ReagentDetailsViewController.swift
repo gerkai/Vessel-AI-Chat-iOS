aca872102c9dd9ad29dd3a605588ae7cb24c70ba
gerkai <gerkai202@gmail.com>
2022-10-13 14:34:35 -0700

    VH-3304: Got back button and scrollView content to look good on devices with or w/o safe area

--- Vessel/Results/ReagentDetailsViewController.swift
+++ Vessel/Results/ReagentDetailsViewController.swift
@@ -7,15 +7,25 @@
 
 import UIKit
 
-class ReagentDetailsViewController: UIViewController
+class ReagentDetailsViewController: UIViewController, UIScrollViewDelegate
 {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        scrollView.delegate = self
     }
     
     @IBAction func back()
     {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView)
+    {
+        //print("Content offset: \(scrollView.contentOffset)")
+        print("Wellness Score: \(titleLabel.frame.origin.y)")
     }
 }
