63ebced4f89b331820253c7ad5fd6970452adeee
gerkai <gerkai202@gmail.com>
2022-12-05 20:34:17 -0800

    VH-4885: Created ObjectLoader module. Moved object loading out of Onboarding code. Screen transitions now occur after all objects loaded.

---
+++ Vessel/Core Objects/ObjectLoader.swift
@@ -0,0 +1,32 @@
+//
+//  ObjectLoader.swift
+//  Vessel
+//
+//  Created by Carson Whitsett on 12/5/22.
+//
+//  This is called after login (or after onboarding) to load all of the core objects from the back end. If the objects already
+//  exist in local storage, the back end will return empty arrays. If stale objects exist locally, the back end will return
+//  updated objects. ObjectStore takes care of storing the objects locally after they've been returned from the back end.
+
+import Foundation
+
+class ObjectLoader: NSObject
+{
+    static let shared = ObjectLoader()
+    
+    func loadCoreObjects(onDone done: @escaping () -> Void)
+    {
+        ObjectStore.shared.getMostRecent(objectTypes: [Result.self, Food.self, Curriculum.self, Plan.self], onSuccess: 
+        {
+            LessonsManager.shared.buildLessonPlan(onDone:
+            {
+                PlansManager.shared.loadPlans()
+                done()
+            })
+        },
+        onFailure:
+        {
+            done()
+        })
+    }
+}
