1a52ab67ed49347e80f6860e6123439482625969
gerkai <gerkai202@gmail.com>
2023-01-13 14:26:08 -0800

    VH-5548: Fixed freeze after going to app store from app

--- Vessel/Review/AppReviewViewController.swift
+++ Vessel/Review/AppReviewViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import SafariServices
 
-class AppReviewViewController: SlideupViewController, VesselScreenIdentifiable
+class AppReviewViewController: SlideupViewController, VesselScreenIdentifiable, SFSafariViewControllerDelegate
 {
     @Resolved internal var analytics: Analytics
     let flowName: AnalyticsFlowName = .appReviewFlow
@@ -34,16 +35,29 @@ class AppReviewViewController: SlideupViewController, VesselScreenIdentifiable
         //move these to Constants if we end up using them elsewhere in the app
         let appId = "1501275949"
         let urlStr = "https://itunes.apple.com/app/id\(appId)?action=write-review"
-        self.openInSafari(url: urlStr)
+        self.openInSafari(url: urlStr, delegate: self)
         analytics.log(event: .appReviewGoToStore(value: true))
-        dismissAnimation
-        {
-        }
     }
     
     @IBAction func onNoThanks()
     {
         analytics.log(event: .appReviewGoToStore(value: false))
+        dismissAnimation
+        {
+        }
+    }
+    
+    // click top left "Done" button and dismissed
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController)
+    {
+        dismissAnimation
+        {
+        }
+    }
+        
+      // swipe down and dismissed
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController)
+    {
         dismissAnimation
         {
         }
