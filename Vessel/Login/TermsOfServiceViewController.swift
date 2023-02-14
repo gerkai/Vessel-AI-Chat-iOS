79eb63cf5541f85e8ff6c3b9f059056d423deeb1
gerkai <gerkai202@gmail.com>
2023-02-14 13:05:07 -0800

    VH-5490: Began adding baked-in ToS content

---
+++ Vessel/Login/TermsOfServiceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TermsOfServiceViewController.swift
+//  Vessel
+//
+//  Created by Carson Whitsett on 2/14/23.
+//
+
+import UIKit
+
+class TermsOfServiceViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
