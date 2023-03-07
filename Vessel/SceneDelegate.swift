ec6683196065539aaeec05a3f5344a0c0742b189
gerkai <gerkai202@gmail.com>
2023-03-07 11:55:26 -0800

    Build 3.0.12(14)

--- Vessel/SceneDelegate.swift
+++ Vessel/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        Log_Add("Scene will connect to: \(connectionOptions)")
-        
-        if let url = connectionOptions.urlContexts.first?.url.absoluteString
+        Log_Add("Scene will connect to: \(String(describing: connectionOptions.urlContexts))")
+        processDynamicLink(url: connectionOptions.urlContexts.first?.url)
+        /*if let url = connectionOptions.urlContexts.first?.url.absoluteString
         {
             Log_Add("URL: \(url)")
             if let expertID = AppDelegate().extractExpertInfo(percentEncodedURLString: url)
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
                 Contact.PractitionerID = expertID
                 Log_Add("3 Setting Global ExpertID: \(expertID)")
             }
-        }
+        }*/
         guard let _ = (scene as? UIWindowScene) else { return }
     }
     
@@ -35,7 +35,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>)
     {
         Log_Add("Scene openURLContexts")
-        if let url = URLContexts.first?.url.absoluteString
+        processDynamicLink(url: URLContexts.first?.url)
+        /*if let url = URLContexts.first?.url.absoluteString
         {
             Log_Add("URL: \(url)")
             if let expertID = AppDelegate().extractExpertInfo(percentEncodedURLString: url)
@@ -44,7 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
                 //if we're already logged in, then go ahead and make the association now
                 makeAssociation(expertID: expertID)
             }
-        }
+        }*/
     }
     
     func scene(_ scene: UIScene, willContinueUserActivityWithType userActivityType: String)
@@ -68,9 +69,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
     func scene(_ scene: UIScene, continue userActivity: NSUserActivity)
     {
         Log_Add("Scene Continue")
-        if let webpageURL = userActivity.webpageURL
+        processDynamicLink(url: userActivity.webpageURL)
+        /*if let webpageURL = userActivity.webpageURL
         {
             let _ = DynamicLinks.dynamicLinks().handleUniversalLink(webpageURL)
+            { dynamiclink, error in
+                Log_Add("Dynamic Link: \(String(describing: dynamiclink)), error: \(String(describing: error))")
+                
+                if let url = dynamiclink?.url
+                {
+                    if let expertID = AppDelegate().extractExpertInfo(percentEncodedURLString: url.absoluteString)
+                    {
+                        self.makeAssociation(expertID: expertID)
+                        Log_Add("5 Setting Global ExpertID: \(expertID)")
+                    }
+                }
+            }
+        }*/
+    }
+    
+    func processDynamicLink(url: URL?)
+    {
+        if let url = url
+        {
+            let _ = DynamicLinks.dynamicLinks().handleUniversalLink(url)
             { dynamiclink, error in
                 Log_Add("Dynamic Link: \(String(describing: dynamiclink)), error: \(String(describing: error))")
                 
