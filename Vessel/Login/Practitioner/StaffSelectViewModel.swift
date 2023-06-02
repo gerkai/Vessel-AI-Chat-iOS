b648f668183b78f2ef0ab45f50575b59b9627546
gerkai <gerkai202@gmail.com>
2023-06-02 02:14:10 -0500

    Change staff id sent to attribution API call

--- Vessel/Login/Practitioner/StaffSelectViewModel.swift
+++ Vessel/Login/Practitioner/StaffSelectViewModel.swift
@@ -8,7 +8,7 @@ class StaffSelectViewModel
     func setSelectedStaff(onCompletion: @escaping () -> ())
     {
         guard let staff = staff[safe: selectedStaff ?? -1] else { return }
-        Server.shared.setExpertAttribution(expertId: staff.expert_id ?? staff.staff_id ?? staff.id, onSuccess: onCompletion) { message in
+        Server.shared.setExpertAttribution(expertId: staff.staff_id ?? staff.id, onSuccess: onCompletion) { message in
             onCompletion()
         }
     }
